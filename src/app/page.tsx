19cc9d08b09679a95e0b8b90c0d283e1f5ca5f30
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-04-12 10:37:17 +0700

    add: graph mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import Map from "./components/map";
 import { Connections, Nodes, Path } from "./lib/utils";
 import { ChangeEvent, use, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import Switch from "react-switch";
+import GraphComponent from "./graph/page";
 import "react-toastify/dist/ReactToastify.css";
 
 interface FormElements extends HTMLFormControlsCollection {
@@ -27,6 +29,7 @@ export default function Home() {
   const [displayRoute, setdisplayRoute] = useState<String>();
   const [displayCost, setdisplayCost] = useState<Number>();
   const [displayTime, setdisplayTime] = useState<String>();
+  const [enabled, setEnabled] = useState(false);
 
   const handleInitialButton = (value: string) => {
     setInitial(value);
@@ -123,11 +126,36 @@ export default function Home() {
 
   return (
     <div className="text-biru h-screen">
-      <Map nodes={nodes!} connections={connections!} path={path} />
+      {enabled === true ? (
+        <GraphComponent nodes={nodes!} connections={connections!} path={path} />
+      ) : (
+        <Map nodes={nodes!} connections={connections!} path={path} />
+      )}
       <ToastContainer />
       <div className="fixed left-20 top-20 z-[100000]">
-        <div className="block max-w-md h-full p-7 bg-card-color">
-          <img src="/findroute.svg"></img>
+        <div className="block max-w-md h-full p-7 bg-card-color space-x-2">
+        <div className="flex space-x-5 space-y-5">
+        <img src="/findroute.svg"></img>
+        <div>
+          <label class="inline-flex relative items-center cursor-pointer">
+            <input
+              type="checkbox"
+              className="sr-only peer"
+              checked={enabled}
+              readOnly
+            />
+            <div
+              onClick={() => {
+                setEnabled(!enabled);
+              }}
+              className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"
+            ></div>
+            <span className="ml-2 text-sm font-medium text-gray-900">
+              Graph
+            </span>
+          </label>
+          </div>
+          </div>
           <div className="flex space-x-14">
             <div className="max-w-lg space-y-4">
               <div className="max-w-xs">
